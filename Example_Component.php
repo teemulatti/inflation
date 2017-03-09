84ab2052b7f6da38f0c8f1897701fe0ea6264196
Teemu Lätti <teemu.latti@cupla.net>
2017-03-09 13:08:43 +0200

    Add files via upload

---
+++ Example_Component.php
@@ -0,0 +1,43 @@
+<html>
+    <!-- This is the component CSS -->
+    <style>
+        
+        .Component {
+            display: inline-block;
+            border: 2px solid #bbb;
+            border-radius: 10px;
+            width: auto;
+            margin: 10px;
+            padding: 10px;
+            font-weight: bold;
+            cursor: default;
+        }
+        .Component:hover {
+            background: rgba(0,0,0,0.05);
+        }
+    
+    </style>
+
+    <!-- This is the component HTML -->
+    <body name="Component" class="Component">
+        <div id="title">Title</div>
+    </body>
+    
+    <!-- This is the component JAVASCRIPT -->
+    <script>
+        
+        function Component(elem) {
+            var that = Inflation.super(this, elem); // always initialize component class like this
+            
+            // Specify your members,methods,etc here
+            
+            this.titleElem = this.getElementById("title");
+            
+            this.setTitle = function(title) {
+                that.titleElem.innerHTML = title;
+            };
+        }
+        
+    </script>
+
+</html>
---
+++ Example_Page.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <script src="inflation.js"></script>
+    </head>
+
+    <body>
+        <!-- This is the page HTML -->
+        
+        <!-- Insert component contents here by inflation -->
+        <div id="comp1" inflate="Component"></div>
+        
+    </body>
+    
+    <script>
+        // Include component file (HTML+CSS+JAVASCRIPT)
+        Inflation.include("Example_Component.php");
+        
+        // This gets called when all included files have been loaded and the page is ready
+        Inflation.ready(function() {
+            
+            // Create component instance and attach to existing (inflated) element
+            var comp1 = new Component(document.getElementById("comp1"));
+            comp1.setTitle("ONE");
+            
+            // Create another instance by inflating here and add it to document programmatically
+            var comp2 = new Component("Component");
+            document.body.appendChild(comp2.elem);
+            comp2.setTitle("TWO");
+            
+        });
+    </script>
+
+</html>
